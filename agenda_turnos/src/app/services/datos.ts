af4df5cb8b85077b5ad503f35fe62e6fb96ee600
GuanacoEnojado <sebastiancastillobri@gmail.com>
2025-06-26 13:16:15 -0400

    jueves 26
    aaa

--- agenda_turnos/src/app/services/datos.ts
+++ agenda_turnos/src/app/services/datos.ts
@@ -3,8 +3,42 @@ export interface trabajador {
   id: number;
   Name1: string;
   Name2: string;
+  fecha_nacimiento: Date;
+  fecha_ingreso: Date;
   email: string;
   turno: string;
+  fechainicioturno: Date;
   contrato: string;
   estado: string;
+  avatarUrl?: string;
 }
+
+export interface User {
+  id?: number;
+  username: string;
+  email: string;
+  password: string;
+  created_at?: string;
+}
+
+export type turno = 
+  | '4to_turno_modificado' 
+  | '3er_turno'
+  | '4to_turno'
+  | 'diurno_hospital'
+  | 'diurno_empresa'
+  | 'volante';
+export type contrato =
+    | 'contrato_indefinido'
+    | 'contrato_fijo'
+    | 'planta'
+    | 'contrata'
+    | 'volante';
+export type estado =
+    | 'activo'
+    | 'inactivo'
+    | 'licencia'
+    | 'vacaciones'
+    | 'suspendido'
+
+
